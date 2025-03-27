fbb806ae1df75cf0694fe872384259e2c9cf9586
Fingsinz <fingsinz@outlook.com>
2025-03-27 19:57:02 +0800

    feat: 添加下载数据集的脚本

---
+++ script/download_content_dataset.sh
@@ -0,0 +1,11 @@
+wget http://images.cocodataset.org/zips/test2017.zip
+
+mv test2017.zip ../datasets
+
+cd ../datasets
+
+unzip test2017.zip
+
+mv test2017 coco2017
+
+rm test2017.zip
---
+++ script/download_style_dataset.sh
@@ -0,0 +1,9 @@
+wget https://ai-studio-online.bj.bcebos.com/v1/64538b8747464a1b9733e28b41006f31d6cffc7c9bdb4eac8683c60ebcae7d61?responseContentDisposition=attachment%3Bfilename%3DWikiArt2.zip&authorization=bce-auth-v1%2F5cfe9a5e1454405eb2a975c43eace6ec%2F2025-03-27T11%3A55%3A39Z%2F21600%2F%2Fd9c82517dcfcbe447760a39dcb39d91d7d428f7e1ec2c36d2d5f959d75dcf198
+
+mv WikiArt2.zip ../datasets
+
+cd ../datasets
+
+unzip WikiArt2.zip
+
+rm WikiArt2.zip
