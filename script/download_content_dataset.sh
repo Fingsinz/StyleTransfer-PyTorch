0c35322d7d8a09727b86072ab5bc9e68695888c8
Fingsinz <fingsinz@outlook.com>
2025-03-27 20:07:26 +0800

    down(dataset): 更新数据集下载脚本

--- script/download_content_dataset.sh
+++ script/download_content_dataset.sh
@@ -1,3 +1,5 @@
+# 国外地址，如果太卡可以替换为飞桨的数据集
+# https://aistudio.baidu.com/datasetdetail/103218 下面的 test2017.zip
 wget http://images.cocodataset.org/zips/test2017.zip
 
 mv test2017.zip ../datasets
--- script/download_style_dataset.sh
+++ script/download_style_dataset.sh
@@ -1,9 +1,11 @@
-wget https://ai-studio-online.bj.bcebos.com/v1/64538b8747464a1b9733e28b41006f31d6cffc7c9bdb4eac8683c60ebcae7d61?responseContentDisposition=attachment%3Bfilename%3DWikiArt2.zip&authorization=bce-auth-v1%2F5cfe9a5e1454405eb2a975c43eace6ec%2F2025-03-27T11%3A55%3A39Z%2F21600%2F%2Fd9c82517dcfcbe447760a39dcb39d91d7d428f7e1ec2c36d2d5f959d75dcf198
+# 国外地址，如果太卡可以替换为飞桨的数据集
+# https://aistudio.baidu.com/datasetdetail/222219 下面的 WikiArt2.zip
+wget https://ia801206.us.archive.org/11/items/WikiArt_dataset/WikiArt_000.tar
 
-mv WikiArt2.zip ../datasets
+mv WikiArt_000.tar ../datasets
 
 cd ../datasets
 
-unzip WikiArt2.zip
+tar -xvf WikiArt_000.tar
 
-rm WikiArt2.zip
+rm WikiArt_000.tar
