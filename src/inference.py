e774cbc0f22638b0d1faaf0567607d90d319f92c
Fingsinz <fingsinz@outlook.com>
2025-04-04 13:11:51 +0800

    feat(inference): 支持批量处理图像

--- src/inference.py
+++ src/inference.py
@@ -50,27 +50,40 @@ def one_image_transfer(content_path, style_path, model_vgg, model_transform, met
     
 if __name__ == "__main__":
     
-    content_img_path = sys.argv[1]
-    style_img_path = sys.argv[2]
+    content_path = sys.argv[1]
+    style_path = sys.argv[2]
     
-    if not os.path.exists(content_img_path) or not os.path.exists(style_img_path):
-        print(f"[ERROR] {content_img_path} or {style_img_path} not exist")
+    if not os.path.exists(content_path) or not os.path.exists(style_path):
+        print(f"[ERROR] {content_path} or {style_path} not exist")
         exit()
     
     vgg16 = VGG16_3_8_15_22().to(Config.device).eval()
     
     load_transform_net = TransformNet(Config.get_base()).to(Config.device)
-    load_model(load_transform_net, './transform_100.pth')
+    load_model(load_transform_net, './transform_attention_epos=100_cw=1_sw=50_tvw=1e-6_interval=20.pth')
     load_transform_net.to(Config.device)
     
     load_metanet = MetaNet(load_transform_net.get_param_dict()).to(Config.device)
-    load_model(load_metanet, './metanet_100.pth')
+    load_model(load_metanet, './metanet_attention_epos=100_cw=1_sw=50_tvw=1e-6_interval=20.pth')
     load_metanet.to(Config.device)
     
-    start = time.perf_counter()
-    one_image_transfer(content_path=content_img_path, style_path=style_img_path,
-                       model_vgg=vgg16, model_transform=load_transform_net, metanet=load_metanet)
-    end = time.perf_counter()
-    print(f"[INFO] 生成图片用时：{end - start} 秒")
+    if os.path.isfile(content_path) and os.path.isfile(style_path):
+        start = time.perf_counter()
+        one_image_transfer(content_path=content_path, style_path=style_path,
+                           model_vgg=vgg16, model_transform=load_transform_net, metanet=load_metanet)
+        end = time.perf_counter()
+        print(f"[INFO] 生成图片用时：{end - start} 秒")
+        exit()
+    else:
+        for content_img_path in os.listdir(content_path):
+            for style_img_path in os.listdir(style_path):
+                start = time.perf_counter()
+                one_image_transfer(content_path=f"{content_path}/{content_img_path}",
+                                   style_path=f"{style_path}/{style_img_path}",
+                                   model_vgg=vgg16, model_transform=load_transform_net, metanet=load_metanet)
+                end = time.perf_counter()
+                print(f"[INFO] 生成图片用时：{end - start} 秒")
+        exit()
+
 
     
