c686e230db8cebf1af9a7a8d2d30622dadaa624f
Fingsinz <fingsinz@outlook.com>
2025-04-27 17:16:34 +0800

    feat(batch_eval): 添加 FID 指标计算功能

--- src/batch_eval.py
+++ src/batch_eval.py
@@ -9,6 +9,9 @@ import torchvision.transforms as transforms
 from utils.utils import calculate_psnr, calculate_ssim, calculate_ms_ssim, gram_matrix, check_dir
 from models.networks import VGG19_Pretrained
 
+from torchvision.models import inception_v3, Inception_V3_Weights
+from scipy.linalg import sqrtm
+
 def cal_batch_ssim_psnr(content_path, transform_path, output_name=""):
     out_dir = check_dir("../output")
     if output_name != "":
@@ -111,24 +114,81 @@ def cal_gram_cosine_similarity(style_path, transform_path, output_name=""):
         f.write(f"Average,,{(total /files):.6f}\n")
     print(f"[INFO] Average cosine similarity: {(total /files):.6f}")
     print(f"[INFO] Metrics saved to {output}")
+
+def cal_fid(content_path, transform_path, output_name=""):
+    out_dir = check_dir("../output")
+    if output_name != "":
+        output = f"{out_dir}/metrics_{output_name}_fid.csv"
+    else:
+        now_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+        output = f"{out_dir}/metrics_fid_{now_time}.csv"
+    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = inception_v3(weights=Inception_V3_Weights.IMAGENET1K_V1, aux_logits=True).eval()
+    model.fc = torch.nn.Identity()
+    model.eval().to(device)
+    
+    preprocess = transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    def get_features(path):
+        feats = []
+        for img in os.listdir(path):
+            img_path = f"{path}/{img}"
+            img = Image.open(img_path).convert('RGB')
+            tensor = preprocess(img).unsqueeze(0).to(device)
+
+            with torch.inference_mode():
+                feat = model(tensor).cpu().numpy().reshape(-1)
+            feats.append(feat)
+        return np.array(feats)
+    
+    content_feats = get_features(content_path)
+    transform_feats = get_features(transform_path)
+    
+    mu1, sigma1 = np.mean(content_feats, axis=0), np.cov(content_feats, rowvar=False)
+    mu2, sigma2 = np.mean(transform_feats, axis=0), np.cov(transform_feats, rowvar=False)
+    
+    diff = mu1 - mu2
+    covmean = sqrtm(sigma1.dot(sigma2))
+    
+    if np.iscomplexobj(covmean):
+        covmean = covmean.real
+    
+    fid = diff.dot(diff) + np.trace(sigma1 + sigma2 - 2 * covmean)
+    
+    with open(output, "w") as f:
+        f.write("fid\n")
+        f.write(f"{fid:.6f}\n")
+    print(f"[INFO] FID: {fid}")
+    print(f"[INFO] FID Metrics saved to {output}")
     
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print(f"[ERROR] Usage: python {sys.argv[0]} <content_path> <style_path> <transform_path>")
+    if len(sys.argv) < 5 or sys.argv[4] not in ["1", "2", "3"]:
+        print(f"[ERROR] Usage: python {sys.argv[0]} <content_path> <style_path> <transform_path> <mode> [output_name]")
+        print(f"[ERROR] mode: 1 -> psnr and ssim, 2 -> Gram 余弦相似度, 3 -> FID")
         exit()
     
     content_path = sys.argv[1]
     style_path = sys.argv[2]
     transform_path = sys.argv[3]
+    mode = sys.argv[4]
     
-    if len(sys.argv) == 5:
-        output_name = sys.argv[4]
+    if len(sys.argv) == 6:
+        output_name = sys.argv[5]
     else:
         output_name = ""
     
     if not os.path.exists(content_path) or not os.path.exists(style_path) or not os.path.exists(transform_path):
-        print(f"[ERROR] {content_path} or {style_path} or {transform_path} not exist")
+        print(f"[ERROR] Path {content_path} or {style_path} or {transform_path} not exist")
         exit()
 
-    cal_batch_ssim_psnr(content_path, transform_path, output_name)
-    cal_gram_cosine_similarity(style_path, transform_path, output_name)
+    if mode == "1":
+        cal_batch_psnr_ssim(content_path, transform_path, output_name)
+    elif mode == "2":
+        cal_batch_gram_cosine_similarity(style_path, transform_path, output_name)
+    elif mode == "3":
+        cal_fid(content_path, transform_path, output_name)
